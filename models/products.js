cc968f72d5e8b2fcdef694f0865cda8577e7e0ef
Stephen B <stephen-bailly_student2021@wilder.school>
2022-02-16 15:10:15 +0100

    models/products added

--- models/products.js
+++ models/products.js
@@ -1,85 +1,43 @@
 const connection = require("../db-config");
-const router = require("express").Router();
+const Joi = require('joi');
 
-router.get('/', (req, res) => {
-    connection.query('SELECT * FROM users', (err, result) => {
-      if (err) {
-        res.status(500).send('Error retrieving users from database');
-      } else {
-        res.json(result);
-      }
-    });
+const db = connection.promise();
+
+const findMany = () => {
+    return db
+    .query('SELECT * FROM products')
+    .then(([results]) => results);
+}
+
+const findOne = (id) => {
+  return db
+  .query('SELECT * FROM products WHERE id = ?', [id])
+  .then(([results]) => results[0]);
+};
+
+const create = ({ProductID, Name, Price, SKU, Characteristic , Description, Ingredient}) => {
+  return db
+  .query("INSERT INTO products (ProductID, Name, Price, SKU, Characteristic , Description, Ingredient) VALUES (?, ?, ?, ?, ?, ?, ?)", [ProductID, Name, Price, SKU, Characteristic , Description, Ingredient])
+  .then(([results]) => {
+    const id = result.insertID;
+    return { ProductID, Name, Price, SKU, Characteristic , Description, Ingredient };
   });
+};
 
-router.get('/:id', (req, res) => {
-  const userId = req.params.id;
-  connection.query(
-    'SELECT * FROM users WHERE id = ?',
-    [userId],
-    (err, results) => {
-      if (err) {
-        res.status(500).send('Error retrieving user from database');
-      } else {
-        if (results.length) res.json(results[0]);
-        else res.status(404).send('User not found');
-      }
-    }
-  );
-});
+const update = (id, newAttributes) => {
+  return db.query('UPDATE products SET ? WHERE id = ?', [newAttributes, id]);
+};
 
-router.post('/', (req, res) => {
-  const { username, password, email } = req.body;
-  connection.query(
-    'INSERT INTO users (username, password, email) VALUES (?, ?, ?)',
-    [username, password, email],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error saving the user');
-      } else {
-        const id = result.insertId;
-        const createdUser = { id, username, password, email };
-        res.status(201).json(createdUser);
-      }
-    }
-  );
-});
+const destroy = (id) => {
+  return db
+    .query('DELETE FROM products WHERE id = ?', [id])
+    .then(([result]) => result.affectedRows !== 0);
+};
 
-router.put('/:id', (req, res) => {
-  const userId = req.params.id;
-  const db = connection.promise();
-  let existingUser = null;
-  db.query('SELECT * FROM users WHERE id = ?', [userId])
-    .then(([results]) => {
-      existingUser = results[0];
-      if (!existingUser) return Promise.reject('RECORD_NOT_FOUND');
-      return db.query('UPDATE users SET ? WHERE id = ?', [req.body, userId]);
-    })
-    .then(() => {
-      res.status(200).json({ ...existingUser, ...req.body });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err === 'RECORD_NOT_FOUND')
-        res.status(404).send(`User with id ${userId} not found.`);
-      else res.status(500).send('Error updating a user');
-    });
-});
-
-router.delete('/:id', (req, res) => {
-  connection.query(
-    'DELETE FROM users WHERE id = ?',
-    [req.params.id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error deleting an user');
-      } else {
-        if (result.affectedRows) res.status(200).send('🎉 User deleted!');
-        else res.status(404).send('User not found.');
-      }
-    }
-  );
-});
-
-module.exports = router;
+module.exports = {
+  findMany,
+  findOne,
+  create,
+  update,
+  destroy
+};

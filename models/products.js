9865eeae04752438166ee172ba20d0736723f20c
Estelle <estelle9.dev@gmail.com>
2022-03-28 11:26:17 +0200

    request validation

--- models/products.js
+++ models/products.js
@@ -15,16 +15,71 @@ const findOne = (id) => {
         .then(([results]) => results[0]);
 };
 
-const create = ({ ProductID, Name, Price, SKU, Characteristic, Description, Ingredient }) => {
+const create = ({
+    ProductID,
+    Name,
+    Price,
+    SKU,
+    Characteristic,
+    Description,
+    Ingredient
+}) => {
+    validationErrors = Joi.object({
+        Name: Joi.string().max(250).required(),
+        Price: Joi.number().max(1000).required(),
+        SKU: Joi.string().max(13).required(),
+        Characteristic: Joi.string().required(),
+        Description: Joi.string().required(),
+        Ingredient: Joi.string().required(),
+    }).validate({
+        Name,
+        Price,
+        SKU,
+        Characteristic,
+        Description,
+        Ingredient
+    }, {
+        abortEarly: false
+    }).error;
+    if (validationErrors) return Promise.reject('INVALID_DATA');
     return db
         .query("INSERT INTO products (ProductID, Name, Price, SKU, Characteristic , Description, Ingredient) VALUES (?, ?, ?, ?, ?, ?, ?)", [ProductID, Name, Price, SKU, Characteristic, Description, Ingredient])
         .then(([results]) => {
             const id = results.insertID;
-            return { id, ProductID, Name, Price, SKU, Characteristic, Description, Ingredient };
+            return {
+                id,
+                ProductID,
+                Name,
+                Price,
+                SKU,
+                Characteristic,
+                Description,
+                Ingredient
+            };
         });
 };
 
 const update = (id, newAttributes) => {
+    validationErrors = Joi.object({
+        ProductID: Joi.number().required(),
+        Name: Joi.string().max(250).required(),
+        Price: Joi.number().max(1000).required(),
+        SKU: Joi.string().max(13).required(),
+        Characteristic: Joi.string().required(),
+        Description: Joi.string().required(),
+        Ingredient: Joi.string().required(),
+    }).validate({
+        ProductID,
+        Name,
+        Price,
+        SKU,
+        Characteristic,
+        Description,
+        Ingredient
+    }, {
+        abortEarly: false
+    }).error;
+    if (validationErrors) return res.status(422).send('INVALID_DATA');
     return db.query('UPDATE products SET ? WHERE id = ?', [newAttributes, id]);
 };
 
